c609a0c095d6d33670c06df819d8142a132ee0f2
Hank Shen <swh@admpub.com>
2023-04-02 14:12:37 +0800

    update

--- collection_ext.go
+++ collection_ext.go
@@ -1,7 +1,24 @@
 package qmgo
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+
+	opts "github.com/webx-top/qmgo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func (c *Collection) Raw() *mongo.Collection {
 	return c.collection
+}
+
+// Find find by condition filter，return QueryI
+func (c *Collection) FindId(ctx context.Context, id interface{}, opts ...opts.FindOptions) QueryI {
+	return &Query{
+		ctx:        ctx,
+		collection: c.collection,
+		filter:     bson.M{"_id": id},
+		opts:       opts,
+		registry:   c.registry,
+	}
 }
